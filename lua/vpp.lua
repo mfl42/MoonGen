865c7b916950f6d9081c294cb0b15c7318ee5185
Michel Besnard <mfl42@mac.com>
2026-03-14 11:04:15 +0100

    Add initial Lua VPP API skeleton

---
+++ lua/vpp.lua
@@ -0,0 +1,19 @@
+local vpp = {}
+
+function vpp.connect()
+    error("not implemented")
+end
+
+function vpp.disconnect()
+    error("not implemented")
+end
+
+function vpp.session_connect(opts)
+    error("not implemented")
+end
+
+function vpp.send(opts)
+    error("not implemented")
+end
+
+return vpp
